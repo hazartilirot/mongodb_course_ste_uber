60b171c5a9dd29d2600d13422400d435a6ba76d0
hazartilirot <hazartilirot@gmail.com>
2021-07-14 08:19:29 +0200

    an express boilerplate being written with a initial mocha test

---
+++ index.mjs
@@ -0,0 +1,3 @@
+import app from './app.mjs'
+
+app.listen(3050, () => console.log('Running on port 3050'))
---
+++ test/app_test.mjs
@@ -0,0 +1,14 @@
+import assert from 'assert'
+import request from 'supertest'
+import app from '../app.mjs'
+
+describe('The express app', () => {
+  it('handles a GET request to api/', done => {
+    request(app)
+      .get('/api')
+      .end((err, res) => {
+        assert(res.body.hi === 'there');
+        done();
+      })
+  })
+})
